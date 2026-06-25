3fa1c812545524d379c64e0fcecbba696c40e3b3
Ricardo Modenese <ricardomodenese2002@gmail.com>
2026-06-25 12:01:39 -0300

    :lipstick: feat: design system no tailwind

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">Fala, grupo! O frontend do dishd tá on!</h1>
-    </main>
-  );
-}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-primary/50 text-white">
+      <h1 className="text-4xl font-bold bg-red-500 p-4 rounded-lg">
+        Teste do Tailwind
+      </h1>
+    </main>
+  );
+}
