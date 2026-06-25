854c510c3cb4fef03c8190a2fb4a68a3b9d47ef7
Ricardo Modenese <ricardomodenese2002@gmail.com>
2026-06-25 10:38:23 -0300

    :bricks: ci: Preparacao do ambiente front

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Fala, grupo! O frontend do dishd tá on!</h1>
+    </main>
+  );
+}
